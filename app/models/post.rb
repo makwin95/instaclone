362c559388f3ff99fd2445ee9becfe2ff4653e0c
Nazarii Makohin <nazariimakohin@gmail.com>
2025-05-22 21:00:26 +0300

    Update to v1.1

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,21 @@
 class Post < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 5, maximum: 100 }
-  validates :description, presence: true,
-                          length: { minimum: 5, maximum: 100 }
-  validates :keywords, presence: true,
-                       length: { minimum: 5, maximum: 100 }
+  validates :description, length: {maximum: 100}
+  validates :keywords, length: {maximum: 100}
 
   has_many_attached :images
+
+  belongs_to :user
+
+  before_create :randomize_id
+
+  private
+
+  def randomize_id
+    loop do
+      self.id = SecureRandom.random_number(1_000_000_000)
+      break unless User.where(id: self.id).exists?
+    end
+  end
 end
