06b234d6da8eca38c27840857de91c012e2b68aa
Nazarii Makohin <nazariimakohin@gmail.com>
2025-05-22 05:26:52 +0300

    Update v1.0

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# This module contains helper methods for the PostsController.
 module PostsHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# This file defines the ApplicationMailer class, which inherits from ActionMailer::Base.
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
